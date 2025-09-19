7b87b473121ceaf6a5d5f71f5085362e25fe7309
Emanuel0428 <cod2877@gmail.com>
2025-09-19 16:05:13 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
